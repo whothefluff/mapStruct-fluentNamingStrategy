5679a49d13fb48e45dc25386ee3037fb423bbba8
Francisco Alcaraz <whothefluff@gmail.com>
2023-12-19 23:08:31 +0100

    Project init

---
+++ src/test/java/com/magnetys/AppTest.java
@@ -0,0 +1,19 @@
+package com.magnetys;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertThat( true ).isEqualTo( true );
+    }
+}
